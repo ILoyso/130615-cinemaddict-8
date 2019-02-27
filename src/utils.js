121b4d108a907e53d7febd7b28ea01ce4f7808dd
Irina Shasheva <loyrina@gmail.com>
2019-02-27 18:14:19 +0300

    Create structure for film, update render function

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,29 @@
 // Random number - not including max value
 export const generateRandomNumber = (max, min = 0) => Math.floor(Math.random() * (max - min)) + min;
 
+export const generateRandomBoolean = () => [true, false][generateRandomNumber(2)];
+
+export const getRandomValue = (values) => values[generateRandomNumber(values.length)];
+
+export const getRandomArrayElements = (array, amount) => {
+  let newArray = [];
+
+  while (amount > 0) {
+    newArray.push(array[generateRandomNumber(array.length)]);
+    amount--;
+  }
+  return newArray;
+};
+
 // Render template befor parent first child
 export const renderTemplate = (parent, template) => {
   parent.insertAdjacentHTML(`afterbegin`, template);
+};
+
+export const Time = {
+  YEAR: 365,
+  DAY: 24,
+  HOUR: 60,
+  MINUTE: 60,
+  SECOND: 1000
 };
