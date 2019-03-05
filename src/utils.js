3ac7459c18e3074ed68448226a5ffcbac2bcf894
Irina Shasheva <loyrina@gmail.com>
2019-03-05 12:06:23 +0300

    Optimize generateRandomBoolean function

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 // Random number - not including max value
 export const generateRandomNumber = (max, min = 0) => Math.floor(Math.random() * (max - min)) + min;
 
-export const generateRandomBoolean = () => [true, false][generateRandomNumber(2)];
+export const generateRandomBoolean = () => generateRandomNumber(2) === 0;
 
 export const getRandomValue = (values) => values[generateRandomNumber(values.length)];
 
